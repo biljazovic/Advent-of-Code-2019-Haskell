44d96f5f5e5cc2edf6e8af00b3954d7eccd12569
Bruno Iljazovic <bruno.iljazovic@gmail.com>
2019-12-01 12:42:00 +0100

    day 1 solution

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,32 +1,21 @@
 module Day1 (main1) where
 
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Maybe
-import Data.List
-import Debug.Trace
-import Control.Applicative (liftA2)
-import Data.Ord
-
-type Pair = (Int, Int)
-
 type IT = [Int]
 
 parse :: String -> IT
 parse = map read . words
 
-solve1 :: IT -> String
-solve1 xs = unwords $ map show sorted where
-  sorted = sortBy (comparing Down) xs
+f :: Int -> Int
+f x = x `div` 3 - 2
+
+solve1 :: IT -> Int
+solve1 = sum . map f
 
 solve2 :: IT -> Int
-solve2 = undefined
+solve2 = sum . concatMap (takeWhile (> 0) . drop 1 . iterate f)
 
 main1 :: IO ()
 main1 = do
   input <- parse <$> readFile "res/input1"
   print $ solve1 input
-  --print $ solve2 input 
-
+  print $ solve2 input 
