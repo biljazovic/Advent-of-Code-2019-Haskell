026ad75e1577f8b2b9fab1eea8f07a725d4aafe2
bruno <comosus7@gmail.com>
2019-12-16 22:30:38 +0100

    day 16 part 2

--- src/Day16.hs
+++ src/Day16.hs
@@ -4,7 +4,7 @@ module Day16 (main16) where
 import Data.Char (digitToInt)
 import Data.Matrix (toList, getCol, colVector, nrows, ncols, fromLists, Matrix(..), identity)
 import Data.Vector (fromList, Vector(..))
-import Data.List (iterate')
+import Data.List (iterate', intersperse)
 
 type IT = [Int]
 
@@ -18,34 +18,24 @@ step n = fromLists $ map f [1..n] where
 modProduct :: Integral a => a -> Matrix a -> Matrix a -> Matrix a
 modProduct p !m1 !m2 = fmap ((`mod` p) . abs) $! m1 * m2
 
-modExp :: Integral a => a -> a -> Matrix a -> Matrix a
-modExp p x m0
-  | x == 0 = identity (nrows m0)
-  | otherwise = let (y, ost) = x `divMod` 2
-                    m1 = modExp p y m0
-                    m2 = modProduct p m1 m1
-                in if ost == 0 then m2 else modProduct p m2 m0
-
---solve1 :: IT -> Vector Int
-solve1 initial = toList finalVec where
+solve1 :: IT -> [Int]
+solve1 initial = take 8 $ toList finalVec where
   initialVec = fromList initial
   n = length initial
-  finalVec = go 1 (colVector initialVec)
+  finalVec = go 100 (colVector initialVec)
   stepn = step n
   go !x !v 
     | x == 0 = v
     | otherwise = go (x-1) $! modProduct 10 stepn v
-    -- | otherwise = modProduct 10 stepn $ go (x-1) v
-  --finalVec = iterate (modProduct 10 (stepn)) (colVector initialVec) !! 100
-  --finalVec = modProduct 10 (modExp 10 1 (step n)) (colVector vec)
-  --finalVec = modProduct 10 ((step n) ^ 100) (colVector vec)
-  
 
-solve2 :: IT -> Int
-solve2 = undefined
+solve2 :: IT -> [Int]
+solve2 initial = take 8 finalList where
+  finalList = iterate' (scanr1 (\x y -> (x + y) `mod` 10)) initList !! 100
+  initList = drop n $ concat $ replicate 10000 initial
+  n = foldl (\s d -> s * 10 + d) 0 $ take 7 initial
 
 main16 :: IO ()
 main16 = do
   input <- parse <$> readFile "res/input16"
   print $ solve1 input
-  --print $ solve2 input 
+  print $ solve2 input 
