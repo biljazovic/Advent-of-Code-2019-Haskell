ef39d8434481e3ac548c8db7689ab2fda92cb002
Bruno Iljazovic <bruno.iljazovic@gmail.com>
2019-12-17 17:45:20 +0100

    day 16 refactor

--- src/Day16.hs
+++ src/Day16.hs
@@ -2,8 +2,6 @@
 module Day16 (main16) where
 
 import Data.Char (digitToInt)
-import Data.Matrix (toList, getCol, colVector, nrows, ncols, fromLists, Matrix(..), identity)
-import Data.Vector (fromList, Vector(..))
 import Data.List (iterate', intersperse)
 
 type IT = [Int]
@@ -11,22 +9,14 @@ type IT = [Int]
 parse :: String -> IT
 parse = map digitToInt . head . lines
 
-step :: Int -> Matrix Int
-step n = fromLists $ map f [1..n] where
-  f x = take n . tail . concatMap (replicate x) $ cycle [0,1,0,-1]
-
-modProduct :: Integral a => a -> Matrix a -> Matrix a -> Matrix a
-modProduct p !m1 !m2 = fmap ((`mod` p) . abs) $! m1 * m2
+step :: [Int] -> [Int]
+step xs = map f (zip xs [1..]) where
+  f (x, i) = (`mod` 10) . abs . sum $ zipWith (*) xs (tail $ concatMap (replicate i) $ cycle [0,1,0,-1])
+  n = length xs
 
 solve1 :: IT -> [Int]
-solve1 initial = take 8 $ toList finalVec where
-  initialVec = fromList initial
-  n = length initial
-  finalVec = go 100 (colVector initialVec)
-  stepn = step n
-  go !x !v 
-    | x == 0 = v
-    | otherwise = go (x-1) $! modProduct 10 stepn v
+solve1 initial = take 8 finalList where
+  finalList = iterate' step initial !! 100
 
 solve2 :: IT -> [Int]
 solve2 initial = take 8 finalList where
